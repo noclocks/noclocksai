4ac8db1e3ba4c6a2132cc44aa60f5cd3bc944e03
Jimmy Briggs <jimmy.briggs@noclocks.dev>
2025-03-03 19:34:10 -0500

    feat: agents and chats

---
+++ R/chats.R
@@ -0,0 +1,57 @@
+
+#  ------------------------------------------------------------------------
+#
+# Title : AI Chats
+#    By : Jimmy Briggs
+#  Date : 2025-03-03
+#
+#  ------------------------------------------------------------------------
+
+#' Initialize AI Chat
+#'
+#' @description
+#' Initialize an AI chat via [ellmer::chat_openai()] and optionally register tools.
+#'
+#' @param model The model to use for the chat. Default is `gpt-4o`.
+#' @param system_prompt The system prompt to use for the chat. Default is `prompt_default_sys()`.
+#' @param tools A list of tools to register with the chat.
+#' @param api_key The OpenAI API key to use for the chat. Default is `get_openai_api_key()`.
+#' @param ... Additional arguments to pass to [ellmer::chat_openai()].
+#'
+#' @returns
+#' An [ellmer::Chat] S7 object.
+#'
+#' @export
+#'
+#' @seealso [ellmer::chat_openai()]
+#'
+#' @importFrom ellmer chat_openai
+#'
+#' @examples
+#' \dontrun{
+#' chat <- initialize_chat()
+#' chat$chat("What is the meaning of life?")
+#' }
+initialize_chat <- function(
+    model = "gpt-4o",
+    system_prompt = prompt_default_sys(),
+    tools = NULL,
+    api_key = get_openai_api_key(),
+    ...
+) {
+
+  chat <- ellmer::chat_openai(
+    model = model,
+    system_prompt = system_prompt,
+    api_key = api_key,
+    ...
+  )
+
+  if (!is.null(tools)) {
+    register_tools(chat, tools)
+  }
+
+  return(chat)
+
+}
+
