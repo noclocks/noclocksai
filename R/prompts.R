542d32a42150e0abbd8a7528edb4647ea41748ef
Jimmy Briggs <jimmy.briggs@noclocks.dev>
2025-03-03 19:34:52 -0500

    feat: prompt template functions

--- R/prompts.R
+++ R/prompts.R
@@ -7,6 +7,32 @@
 #
 #  ------------------------------------------------------------------------
 
+#' Prompts
+#'
+#' @name prompts
+#'
+#' @description
+#' A collection of functions for generating prompts for the chat agent.
+#'
+#' Functions:
+#'
+#' - `prompt_default_sys()`: Generate the default system prompt for the chat session.
+#'
+#' @param ... Additional arguments passed to the template as data.
+#'
+#' @returns
+#' - `prompt_default_sys()`: A character string representing the default system prompt.
+#'
+#' @seealso [ellmer::interpolate_file()]
+NULL
+
+#' @rdname prompts
+#' @export
+#' @importFrom ellmer interpolate_file
+prompt_default_sys <- function(...) {
+  ellmer::interpolate_file(path = pkg_sys("prompts/default.system.prompt.md"))
+}
+
 prompt_pkgdown_sys <- function() {
   ellmer::interpolate_file(path = pkg_sys("prompts/pkgdown.system.prompt.md"))
 }
