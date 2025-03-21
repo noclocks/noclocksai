0d126a80854830e150d5d727f183f6c5f8066954
Jimmy Briggs <jimmy.briggs@noclocks.dev>
2025-03-20 22:35:49 -0400

    feat(docs): add package metadata and global variables
    Enhance the package documentation by including metadata such as title, author, and date. Additionally, define global variables for better namespace management.
    
    No Breaking Changes

--- R/noclocksai-package.R
+++ R/noclocksai-package.R
@@ -1,9 +1,34 @@
+
+#  ------------------------------------------------------------------------
+#
+# Title : Pacakge
+#    By : Jimmy Briggs
+#  Date : 2025-03-14
+#
+#  ------------------------------------------------------------------------
+
+# docs --------------------------------------------------------------------
+
 #' @keywords internal
 "_PACKAGE"
 
 ## usethis namespace: start
-#' @importFrom rlang %||%
-#' @importFrom rlang .data
-#' @importFrom rlang .env
+#' @import rlang
 ## usethis namespace: end
 NULL
+
+
+# globals ---------------------------------------------------------------------------------------------------------
+
+utils::globalVariables(
+  c(
+
+  )
+)
+
+# env ---------------------------------------------------------------------
+
+.pkg_env <- rlang::new_environment()
+.pkg_env$pkg_name <- "noclocksai"
+.pkg_env$pkg_version <- utils::packageVersion("noclocksai")
+# .pkg_env$pkg_config_file <- noclocksai:::cfg
