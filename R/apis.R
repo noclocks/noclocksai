f9671b6336814a6b8bfe63266dc414f765805f3d
Jimmy Briggs <jimmy.briggs@noclocks.dev>
2025-04-01 16:46:06 -0400

    refactor(api): reorganize OpenWeather API functions
    Move geocoding and weather functions to a dedicated openweather.R file
    and rename functions with `ow_` prefix for clarity. Streamline API handling
    by removing redundant helper functions.
    
    This restructuring improves code organization and enhances future
    maintainability by clearly separating OpenWeather API-related logic.

--- R/apis.R
+++ R/apis.R
@@ -47,79 +47,8 @@ get_api_url <- function(provider, name = NULL) {
 }
 
 
-# geocode ---------------------------------------------------------------------------------------------------------
 
-#' Geocode
-#'
-#' @description
-#' Geocode a location using the OpenWeather API.
-#'
-#' @param location A character string representing the location to geocode.
-#' @param api_key A character string representing the OpenWeather API key.
-#'
-#' @returns
-#' A list containing the geocoded location data.
-#'
-#' @export
-#'
-#' @importFrom httr2 request req_url_query req_perform resp_check_status resp_body_json
-geocode <- function(location, api_key = get_api_key("openweather")) {
 
-  base_url <- "http://api.openweathermap.org/geo/1.0/direct"
 
-  query_params <- list(
-    q = location,
-    limit = 1,
-    appid = api_key
-  )
-
-  req <- httr2::request(base_url) |>
-    httr2::req_url_query(!!!query_params)
-
-  resp <- httr2::req_perform(req)
-  httr2::resp_check_status(resp)
-
-  httr2::resp_body_json(resp)
-
-}
-
-# get weather -----------------------------------------------------------------------------------------------------
-
-#' Get Weather
-#'
-#' @description
-#' Get the current weather for a given location using the OpenWeather API.
-#'
-#' @param location A character string representing the location to get the weather for.
-#' @param api_key A character string representing the OpenWeather API key.
-#'   If not provided, it will be retrieved from the environment variable `OPENWEATHER_API_KEY`.
-#'
-#' @returns
-#' A list containing the current weather data for the specified location.
-#'
-#' @export
-get_weather <- function(lat, lon, units = c("standard", "metric", "imperial"), api_key = get_api_key("openweather")) {
-
-  units <- match.arg(units)
-  if (units == "standard") units <- NULL
-
-  base_url <- "https://api.openweathermap.org/data/2.5/weather"
-
-  query_params <- list(
-    lat = lat,
-    lon = lon,
-    units = units,
-    appid = api_key
-  ) |>
-    purrr::compact()
-
-  req <- httr2::request(base_url) |>
-    httr2::req_url_query(!!!query_params)
-
-  resp <- httr2::req_perform(req)
-  httr2::resp_check_status(resp)
-
-  httr2::resp_body_json(resp)
-}
 
 
